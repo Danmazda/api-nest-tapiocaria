c0b0493a570359c7aa787f782fc17a26ac4387ec
Danilo Procopio <dan_mazda@hotmail.com>
2022-06-20 08:29:11 -0300

    chore: change from port `3000` to `3333`

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('/hello')
-  getHello(): { message: string } {
-    return this.appService.getHello();
+  getAppStatus(): { message: string } {
+    return this.appService.getAppStatus();
   }
 }
