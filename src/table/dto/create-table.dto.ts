51de202fd865773cca1ac57b71f43019018af1d5
Danilo Procopio <dan_mazda@hotmail.com>
2022-06-20 11:11:08 -0300

    feat(prisma): `Table` model implemented

--- src/table/dto/create-table.dto.ts
+++ src/table/dto/create-table.dto.ts
@@ -1,3 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsPositive } from 'class-validator';
 export class CreateTableDto {
+  @ApiProperty({
+    description: 'O número da mesa.',
+    example: 1,
+  })
+  @IsNumber()
+  @IsPositive()
   number: number;
 }
