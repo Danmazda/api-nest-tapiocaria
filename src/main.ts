293e74c74a799f15f4e38948deeb989741427caf
Danilo Procopio <dan_mazda@hotmail.com>
2022-06-27 18:55:03 -0300

    fix: listening on env port

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3333);
+  await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
